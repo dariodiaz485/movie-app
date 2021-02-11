4f2ab290ab9c135f7ecdcdc5ab3ebd72da75a6ab
Dario Diaz <dariopista@icloud.com>
2021-02-11 02:08:58 -0500

    busqueda de peliculas

--- script.js
+++ script.js
@@ -3,35 +3,48 @@ const APIURL =
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-const main = document.querySelector("main");
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-async function getMovies() {
-  const resp = await fetch(APIURL);
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+//initially get dav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const resp = await fetch(url);
   const respData = await resp.json();
 
   console.log(respData);
 
-  respData.results.forEach((movie) => {
+  showMovies(respData.results);
+}
+
+function showMovies(movies) {
+  //clear main
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
     const { poster_path, title, vote_average } = movie;
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-      
-        <img
-          src="${IMGPATH + poster_path}"
-          alt="${title}"
-        />
-        <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}" >${vote_average}</span>
-        </div>`;
+    <img 
+       src="${IMGPATH + poster_path}"
+       alt="${title}"
+       />
+    <div class="movies-info">
+       <h3>${title}</h3>
+       <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+       </div>
+       `;
 
     main.appendChild(movieEl);
   });
-
-  return respData;
 }
 
 function getClassByRate(vote) {
@@ -45,3 +58,15 @@ function getClassByRate(vote) {
 }
 
 getMovies();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+    search.value = "";
+  }
+});
